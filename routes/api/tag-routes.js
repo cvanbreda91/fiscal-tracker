d4a5a6aef2526ce35860d329c8110d2f2711e2f2
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-09-25 19:18:16 -0400

    tag routes working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ['product_name', 'price', 'stock']
+        attributes: ['id','product_name', 'price', 'stock']
       },
     ]
   })
@@ -31,7 +31,7 @@ router.get('/:id', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ['product_name', 'price', 'stock']
+        attributes: ['id','product_name', 'price', 'stock']
       },
     ]
 }).then(tag => {
@@ -49,14 +49,57 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  // expects {tag_name: "country"}
+  Tag.create(req.body)
+  .then((tag) => {
+    res.status(200).json(tag);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  // expects {tag_name: 'country'}
+  Tag.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(tag => {
+      if (!tag[0]) {
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+      }
+      res.json(tag);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(tag=> {
+      if (!tag) {
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+      }
+      res.json(tag);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
